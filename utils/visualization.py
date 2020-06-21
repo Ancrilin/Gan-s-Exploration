66c65a39203f7789caaf8ed2d510079599ee4e5c
Arcilin <1162236967@qq.com>
2020-06-21 20:47:09 +0800

    visualization id_to_label

--- utils/visualization.py
+++ utils/visualization.py
@@ -22,11 +22,13 @@ def scatter_plot(data: np.ndarray, processor: BaseProcessor):
     data: [x, y, label]
     """
     # prepare id to label map
-    id_to_label = ['OOD', 'ID']
     id_to_label_map = {i: label for i, label in enumerate(processor.id_to_label)}
     # id_to_label_map = {i: label for i, label in enumerate(id_to_label)}
     print('id_to_label_map', type(id_to_label_map), id_to_label_map)
     id_to_label_map[-1] = 'GAN'
+    id_to_label_map[0] = 'OOD'
+    id_to_label_map[1] = 'ID'
+    print('id_to_label_map', type(id_to_label_map), id_to_label_map)
     print('id_to_label_map', id_to_label_map)
     df = pd.DataFrame(data, columns=['x', 'y', 'label'])
     df['label'] = df['label'].apply(lambda x: id_to_label_map[int(x)])
