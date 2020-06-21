f00bb478b8f9750b6b42502f58879a7f2f53ce6c
Arcilin <1162236967@qq.com>
2020-06-22 04:26:33 +0800

    ood-gan sh

--- run_sh/run_ood_gan.sh
+++ run_sh/run_ood_gan.sh
@@ -3,26 +3,30 @@
 seeds="16 123 256 512 1024 1536 2048 4096 8192"
 # dataset_file="binary_smp_full_v2"
 for seed in ${seeds} ; do
-  python -m app.run_gan \
-    --D_lr=2e-5 \
-    --G_lr=2e-5 \
-    --bert_lr=2e-5 \
-    --fine_tune \
-    --n_epoch=500 \
-    --patience=10 \
-    --train_batch_size=32 \
-    --bert_type=bert-base-chinese \
-    --dataset=smp \
-    --data_file=$1 \
-    --output_dir=output/ood-gan_s$seed \
-    --do_train \
-    --do_eval \
-    --do_test \
-    --do_vis \
-    --feature_dim=768 \
-    --G_z_dim=1024  \
-    --seed=$seed  \
-    --result=$2
-  rm -rf output/ood-gan_s$seed/save
+  python -m app.run_oodp_gan \
+  --model=gan  \
+  --seed=${seed}  \
+  --D_lr=2e-5 \
+  --G_lr=2e-5 \
+  --bert_lr=2e-5 \
+  --fine_tune \
+  --n_epoch=500 \
+  --patience=10 \
+  --train_batch_size=32 \
+  --bert_type=bert-base-chinese \
+  --dataset=smp \
+  --data_file=$1 \
+  --output_dir=ood-gan/ood-gan-smp_maxlen$3_mode$2_s${seed} \
+  --do_train \
+  --do_eval \
+  --do_test \
+  --do_vis \
+  --feature_dim=768 \
+  --G_z_dim=1024  \
+  --mode=$2  \
+  --maxlen=$3 \
+  --result=$4 \
+  --ood
+  rm -rf ood-gan/ood-gan-smp_maxlen$3_mode$2_s${seed}/save
 done
 exit 0
