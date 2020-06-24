7274f2f98213d9ebd87f64a2203b83fa9fad41d1
Arcilin <1162236967@qq.com>
2020-06-24 16:45:00 +0800

    device minlen

--- run_sh/run_GQOGAN.sh
+++ run_sh/run_GQOGAN.sh
@@ -4,10 +4,11 @@ lengths="21"
 # dataset_file="binary_true_smp_full_v2"
 # $1 dataset_file
 # $2 mode
-# $3 save_path
+# $3 minlen
+# $4 save_path of my computer
 for len in ${lengths} ; do
-  bash run_sh/run_oodp_gan.sh $1 $2 ${len} GQOGAN-maxlen${len}_mode$2
+  bash run_sh/run_oodp_gan.sh $1 $2 ${len} $3 GQOGAN-maxlen${len}_minlen-1_mode$2
 done
 mv oodp-gan GQOGAN_mode$2
-mv GQOGAN_mode$2 $3/GQOGAN_mode$2
+mv GQOGAN_mode$2 $4/GQOGAN_mode$2
 exit 0
--- run_sh/run_oodp_gan.sh
+++ run_sh/run_oodp_gan.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 
 seeds="16 123 256 512 1024 1536 2048 4096 8192"
-# dataset_file="binary_smp_full_v2"
+## dataset_file="binary_true_smp_full_v2"
+## $1 dataset_file
+## $2 mode
+## $3 maxlen
+## $4 minlen
+## $5 gross_result name
 for seed in ${seeds} ; do
   python -m app.run_oodp_gan \
   --model=gan  \
@@ -25,7 +30,8 @@ for seed in ${seeds} ; do
   --G_z_dim=1024  \
   --mode=$2  \
   --maxlen=$3 \
-  --result=$4
+  --minlen=$4 \
+  --result=$5
   rm -rf oodp-gan/oodp-gan-smp_maxlen$3_mode$2_s${seed}/save
 done
 exit 0
