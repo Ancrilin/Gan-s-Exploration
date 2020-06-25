f0ff757894426cee8b3fce27e8d8dcffa85a239f
Arcilin <1162236967@qq.com>
2020-06-25 09:01:06 +0800

    test GQOGAN save

--- run_sh/run_GQOGAN.sh
+++ run_sh/run_GQOGAN.sh
@@ -7,9 +7,9 @@ lengths="15"
 # $3 minlen
 # $4 save_path of my computer
 for len in ${lengths} ; do
-  bash run_sh/run_oodp_gan.sh $1 $2 ${len} $3 GQOGAN-maxlen${len}_minlen-1_mode$2
-  mv oodp-gan GQOGAN_mode$2_maxlen${len}
-  mv GQOGAN-maxlen${len}_minlen-1_mode$2_gross_result.csv GQOGAN_mode$2_maxlen${len}
-  cp -r "/content/Gan-s-Exploration/GQOGAN_mode$2_maxlen${len}" "$4"
+  bash run_sh/run_oodp_gan.sh ${1} ${2} ${len} $3 GQOGAN-maxlen${len}_minlen-1_mode$2
+  mv oodp-gan GQOGAN_mode${2}_maxlen${len}
+  mv GQOGAN-maxlen${len}_minlen-1_mode${2}_gross_result.csv GQOGAN_mode${2}_maxlen${len}
+  cp -r "/content/Gan-s-Exploration/GQOGAN_mode${2}_maxlen${len}" "$4"
 done
 exit 0
