b703545c01659d07ad177fa699adbbba6e936b92
zihengruan <zihengruan@163.com>
2020-07-08 14:01:03 +0800

    edit sh

--- run_sh/run_GQOGAN_minlen_weights.sh
+++ run_sh/run_GQOGAN_minlen_weights.sh
@@ -8,11 +8,12 @@ weights="0.2 0.4 0.6 0.8"
 # $4 minlen
 # $5 optim_mode
 # $6 length_weight
-# $7 save_path of my computer
+# $7 sample_weight
+# $8 save_path of my computer
 for weight in ${weights} ; do
-  bash run_sh/run_oodp_gan_length.sh ${1} ${2} ${3} ${4} ${5} ${weight} GQOGAN-maxlen${3}_minlen${4}_mode${2}_length_mode${5}_weight${weight}
-  mv oodp-gan GQOGAN_mode${2}_maxlen${3}_minlen${4}_length_mode${5}_weight${weight}
-  mv GQOGAN-maxlen${3}_minlen${4}_mode${2}_length_mode${5}_weight${weight}_gross_result.csv GQOGAN_mode${2}_maxlen${3}_minlen${4}_length_mode${5}_weight${weight}
-  cp -r "/content/Gan-s-Exploration/GQOGAN_mode${2}_maxlen${3}_minlen${4}_length_mode${5}_weight${weight}" "$7"
+  bash run_sh/run_oodp_gan_length.sh ${1} ${2} ${3} ${4} ${5} ${weight} ${7} GQOGAN-maxlen${3}_minlen${4}_mode${2}_optim_mode${5}_ls${weight}_sw${7}
+  mv oodp-gan GQOGAN_mode${2}_maxlen${3}_minlen${4}_optim_mode${5}_ls${weight}_sw${7}
+  mv GQOGAN-maxlen${3}_minlen${4}_mode${2}_optim_mode${5}_ls${weight}_sw${7}_gross_result.csv oodp-gan GQOGAN_mode${2}_maxlen${3}_minlen${4}_optim_mode${5}_ls${weight}_sw${7}
+  cp -r "/content/Gan-s-Exploration/GQOGAN_mode${2}_maxlen${3}_minlen${4}_optim_mode${5}_ls${weight}_sw${7}" "$8"
 done
 exit 0
